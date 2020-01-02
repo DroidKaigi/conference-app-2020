d362f8ca30a04ac8048568c99e25c65c226ae360
takahirom <takam.dev@gmail.com>
2020-01-02 20:30:58 +0900

    Use KTX asLiveData

--- feature/staff/src/main/java/io/github/droidkaigi/confsched2020/staff/ui/viewmodel/StaffsViewModel.kt
+++ feature/staff/src/main/java/io/github/droidkaigi/confsched2020/staff/ui/viewmodel/StaffsViewModel.kt
@@ -2,8 +2,8 @@ package io.github.droidkaigi.confsched2020.staff.ui.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.liveData
-import io.github.droidkaigi.confsched2020.ext.asLiveData
 import io.github.droidkaigi.confsched2020.ext.toLoadingState
 import io.github.droidkaigi.confsched2020.model.repository.StaffRepository
 import io.github.droidkaigi.confsched2020.model.LoadState
