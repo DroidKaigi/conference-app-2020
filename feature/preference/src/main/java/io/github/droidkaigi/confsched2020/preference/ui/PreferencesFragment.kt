ec8558ac63a588dcbdb56e6b2f0957fb5ec16061
FujiKinaga <kinaga@nana-music.com>
2020-01-09 01:21:51 +0900

    Add DarkTheme function

--- feature/preference/src/main/java/io/github/droidkaigi/confsched2020/preference/ui/PreferencesFragment.kt
+++ feature/preference/src/main/java/io/github/droidkaigi/confsched2020/preference/ui/PreferencesFragment.kt
@@ -2,20 +2,43 @@ package io.github.droidkaigi.confsched2020.preference.ui
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 import dagger.Component
 import dagger.Module
 import dagger.Provides
 import io.github.droidkaigi.confsched2020.App
 import io.github.droidkaigi.confsched2020.di.AppComponent
 import io.github.droidkaigi.confsched2020.di.PageScope
+import io.github.droidkaigi.confsched2020.preference.R
 import io.github.droidkaigi.confsched2020.util.ProgressTimeLatch
 
 class PreferencesFragment : PreferenceFragmentCompat() {
+
+    var darkThemeSwitchChangeListener =
+        Preference.OnPreferenceChangeListener { _, newValue ->
+            AppCompatDelegate.setDefaultNightMode(
+                if ((newValue as Boolean)) {
+                    MODE_NIGHT_YES
+                } else {
+                    MODE_NIGHT_NO
+                }
+            )
+            return@OnPreferenceChangeListener true
+        }
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-//        setPreferencesFromResource()
+        setPreferencesFromResource(R.xml.setting, rootKey)
+
+        val darkThemeSwitch =
+            preferenceManager.findPreference(SWITCH_DARK_THEME_KEY) as? SwitchPreferenceCompat
+        darkThemeSwitch?.onPreferenceChangeListener = darkThemeSwitchChangeListener
     }
 
     private lateinit var progressTimeLatch: ProgressTimeLatch
@@ -29,6 +52,10 @@ class PreferencesFragment : PreferenceFragmentCompat() {
             .preferenceModule(PreferenceModule(this))
             .build()
         component.inject(this)
+    }
+
+    companion object {
+        private const val SWITCH_DARK_THEME_KEY = "switchDarkTheme"
     }
 }
 
