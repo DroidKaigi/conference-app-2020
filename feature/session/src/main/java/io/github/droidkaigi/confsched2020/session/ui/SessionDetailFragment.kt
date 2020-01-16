2b427d49520689950d4f05c4a87d7f5ff3b4a4fc
syo-sa1982 <fujii1308@gmail.com>
2020-01-17 00:01:06 +0900

    Add text view collapse - reference from: https://github.com/DroidKaigi/conference-app-2019/blob/master/feature/session/src/main/java/io/github/droidkaigi/confsched2019/session/ui/SessionDetailFragment.kt#L235

--- feature/session/src/main/java/io/github/droidkaigi/confsched2020/session/ui/SessionDetailFragment.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2020/session/ui/SessionDetailFragment.kt
@@ -2,11 +2,20 @@ package io.github.droidkaigi.confsched2020.session.ui
 
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.TextPaint
+import android.text.TextUtils
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.view.doOnPreDraw
+import androidx.core.content.ContextCompat
+import androidx.core.text.buildSpannedString
+import androidx.core.text.color
+import androidx.core.text.inSpans
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleOwner
@@ -14,6 +23,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.transition.TransitionManager
 import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
 import coil.Coil
 import coil.api.load
@@ -42,6 +52,8 @@ import io.github.droidkaigi.confsched2020.util.ProgressTimeLatch
 import javax.inject.Inject
 import javax.inject.Provider
 
+private const val ELLIPSIS_LINE_COUNT = 6
+
 class SessionDetailFragment : DaggerFragment() {
 
     private lateinit var binding: FragmentSessionDetailBinding
@@ -59,6 +71,7 @@ class SessionDetailFragment : DaggerFragment() {
     @Inject lateinit var sessionItemFactory: SessionItem.Factory
 
     private lateinit var progressTimeLatch: ProgressTimeLatch
+    private var showEllipsis = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -126,7 +139,49 @@ class SessionDetailFragment : DaggerFragment() {
         val textView = binding.sessionDescription
         textView.doOnPreDraw {
             textView.text = fullDescription
+            //Return here if not more than the specified number of rows
+            if (!(textView.lineCount > ELLIPSIS_LINE_COUNT && showEllipsis)) return@doOnPreDraw
+            val lastLineStartPosition = textView.layout.getLineStart(ELLIPSIS_LINE_COUNT - 1)
+            val ellipsis = getString(R.string.ellipsis_label)
+            val lastLineText = TextUtils.ellipsize(
+                fullDescription.substring(lastLineStartPosition),
+                textView.paint,
+                textView.width - textView.paint.measureText(ellipsis),
+                TextUtils.TruncateAt.END
+            )
+            val ellipsisColor = ContextCompat.getColor(requireContext(), R.color.design_default_color_secondary)
+            val onClickListener = {
+                TransitionManager.beginDelayedTransition(binding.sessionLayout)
+                textView.text = fullDescription
+                showEllipsis = !showEllipsis
+            }
+            val detailText = fullDescription.substring(0, lastLineStartPosition) + lastLineText
+            val text = buildSpannedString {
+                clickableSpan(onClickListener, {
+                    append(detailText)
+                    color(ellipsisColor) {
+                        append(ellipsis)
+                    }
+                })
+            }
+            textView.setText(text, TextView.BufferType.SPANNABLE)
+            textView.movementMethod = LinkMovementMethod.getInstance()
         }
+    }
+
+    private fun SpannableStringBuilder.clickableSpan(
+        clickListener: () -> Unit,
+        builderAction: SpannableStringBuilder.() -> Unit
+    ) {
+        inSpans(object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                clickListener()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                // nothing
+            }
+        }, builderAction)
     }
 
     private fun ViewGroup.bindSpeaker(session: Session) {
