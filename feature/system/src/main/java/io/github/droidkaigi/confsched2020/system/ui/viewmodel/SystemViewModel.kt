b94ebbdb6e953183abf4a5a8133c01573ae90942
furusin <furusin.oriver@gmail.com>
2020-01-16 23:01:28 +0900

    Add: adding openCalendar function

--- feature/system/src/main/java/io/github/droidkaigi/confsched2020/system/ui/viewmodel/SystemViewModel.kt
+++ feature/system/src/main/java/io/github/droidkaigi/confsched2020/system/ui/viewmodel/SystemViewModel.kt
@@ -2,6 +2,7 @@ package io.github.droidkaigi.confsched2020.system.ui.viewmodel
 
 import android.content.Intent
 import android.net.Uri
+import android.provider.CalendarContract
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.LiveData
@@ -34,5 +35,21 @@ class SystemViewModel @Inject constructor(
 
         // launch a Custom Tabs Activity
         customTabsIntent.launchUrl(activity, Uri.parse(url))
+    }
+
+
+    fun openCalendar(
+        title: String,
+        location: String,
+        startUnixMillis: Long,
+        endUnixMillis: Long
+    ) {
+        val intent = Intent(Intent.ACTION_INSERT)
+            .setData(CalendarContract.Events.CONTENT_URI)
+            .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startUnixMillis)
+            .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endUnixMillis)
+            .putExtra(CalendarContract.Events.TITLE, "DroidKaigi2020: $title")
+            .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+        activity.startActivity(intent)
     }
 }
