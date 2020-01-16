cbef9253b114ad7170af2d06e289e8e1bb578824
sdsd08013 <sd08013+git@gmail.com>
2020-01-17 00:42:56 +0900

    add search sessions viewmodel test

---
+++ feature/session/src/test/java/io/github/droidkaigi/confsched2020/session/ui/viewmodel/SearchSessionsViewModelTest.kt
@@ -0,0 +1,20 @@
+package io.github.droidkaigi.confsched2020.session.ui.viewmodel
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SearchSessionsViewModelTest {
+
+    @Test
+    fun getUiModel() {
+    }
+
+    @Test
+    fun updateSearchQuery() {
+    }
+
+    @Test
+    fun getSessionRepository() {
+    }
+}
