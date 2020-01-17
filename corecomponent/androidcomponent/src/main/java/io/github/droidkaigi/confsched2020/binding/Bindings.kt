7d7e5273ced535ad921cc949ed308215fbe10e3a
hunachi <hanahanahunachi@gmail.com>
2020-01-17 23:42:50 +0900

    Create BindingAdapter for hide and gone view.

--- corecomponent/androidcomponent/src/main/java/io/github/droidkaigi/confsched2020/binding/Bindings.kt
+++ corecomponent/androidcomponent/src/main/java/io/github/droidkaigi/confsched2020/binding/Bindings.kt
@@ -1,10 +1,47 @@
 package io.github.droidkaigi.confsched2020.binding
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("isVisible")
-fun View.showHide(show: Boolean) {
+fun View.showGone(show: Boolean) {
     isVisible = show
+}
+
+@BindingAdapter("isHide")
+fun View.showHide(show: Boolean) {
+    visibility = if (show) View.VISIBLE else View.INVISIBLE
+}
+
+@BindingAdapter("isVisibleWithAnimation")
+fun View.showGoneWithAnimation(show: Boolean) {
+    val shortAnimationDuration = resources.getInteger(android.R.integer.config_longAnimTime)
+    val endOpacity = if (show) 100f else 0f
+    val endVisible = if (show) View.VISIBLE else View.GONE
+    animate()
+        .alpha(endOpacity)
+        .setDuration(shortAnimationDuration.toLong())
+        .setListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                visibility = endVisible
+            }
+        })
+}
+
+@BindingAdapter("isHideWithAnimation")
+fun View.showHideWithAnimation(show: Boolean) {
+    val shortAnimationDuration = resources.getInteger(android.R.integer.config_longAnimTime)
+    val endOpacity = if (show) 100f else 0f
+    val endVisible = if (show) View.VISIBLE else View.INVISIBLE
+    animate()
+        .alpha(endOpacity)
+        .setDuration(shortAnimationDuration.toLong())
+        .setListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                visibility = endVisible
+            }
+        })
 }
