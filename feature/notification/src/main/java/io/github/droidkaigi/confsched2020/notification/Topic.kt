72f8ae29ccc1955690f6aebbaca270fb0c839618
Daishin16 <sakuya1202@icloud.com>
2020-01-18 14:42:37 +0900

    delete announce

--- feature/notification/src/main/java/io/github/droidkaigi/confsched2020/notification/Topic.kt
+++
@@ -1,6 +0,0 @@
-package io.github.droidkaigi.confsched2020.notification
-
-sealed class Topic(val name: String) {
-    object JaAnnouncement : Topic("jp_announcement")
-    object EnAnnouncement : Topic("en_announcement")
-}
