a64a245c20f041d77a0346100a1f0e8f1663c59c
ryoya ito <horusu.dqx@gmail.com>
2020-01-19 20:13:58 +0900

    delete unnecessary codes

--- ios-base/DroidKaigi 2020/ApplicationScheme.swift
+++ ios-base/DroidKaigi 2020/ApplicationScheme.swift
@@ -46,21 +46,9 @@ final class ApplicationScheme: NSObject {
         let scheme = MDCSemanticColorScheme(defaults: .material201804)
 
         scheme.primaryColor = UIColor(hex: "041E42")
-        scheme.primaryColorVariant =
-            UIColor(red: 68.0/255.0, green: 44.0/255.0, blue: 46.0/255.0, alpha: 1.0)
         scheme.onPrimaryColor = .white
         scheme.secondaryColor = UIColor(hex: "041E42")
-        scheme.onSecondaryColor =
-            UIColor(red: 68.0/255.0, green: 44.0/255.0, blue: 46.0/255.0, alpha: 1.0)
         scheme.surfaceColor = UIColor(hex: "041E42")
-        scheme.onSurfaceColor =
-            UIColor(red: 68.0/255.0, green: 44.0/255.0, blue: 46.0/255.0, alpha: 1.0)
-        scheme.backgroundColor =
-            UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)
-        scheme.onBackgroundColor =
-            UIColor(red: 68.0/255.0, green: 44.0/255.0, blue: 46.0/255.0, alpha: 1.0)
-        scheme.errorColor =
-            UIColor(red: 197.0/255.0, green: 3.0/255.0, blue: 43.0/255.0, alpha: 1.0)
         return scheme
     }()
 
