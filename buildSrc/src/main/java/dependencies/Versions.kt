aa65a268e0af069a754c32233949c09559c192ef
Tatsuya Okayama <honwaka.developer@gmail.com>
2020-01-20 11:04:32 +0900

    Revert "change val to const val as in Packages.kt"
    This reverts commit 809d5fbfb6fc66a849ac79db30bdb1d111bbb30e.

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -1,16 +1,16 @@
 package dependencies
 
 private object Versions {
-    const val androidTargetSdkVersion = 29
-    const val androidCompileSdkVersion = 29
-    const val androidMinSdkVersion = 21
+    val androidTargetSdkVersion = 29
+    val androidCompileSdkVersion = 29
+    val androidMinSdkVersion = 21
 
-    private const val versionMajor = 1
-    private const val versionMinor = 0
-    private const val versionPatch = 5
-    private const val versionOffset = 0
-    const val androidVersionCode =
+    private val versionMajor = 1
+    private val versionMinor = 0
+    private val versionPatch = 5
+    private val versionOffset = 0
+    val androidVersionCode =
         (versionMajor * 10000 + versionMinor * 100 + versionPatch) * 100 + versionOffset
 
-    const val androidVersionName = "$versionMajor.$versionMinor.$versionPatch"
+    val androidVersionName = "$versionMajor.$versionMinor.$versionPatch"
 }
