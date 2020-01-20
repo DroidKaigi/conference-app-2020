3278f526792b9d2bb12a2eb5b4f5c3bcaf3519ad
takada <haruhiko.takada@gmail.com>
2020-01-20 21:33:08 +0900

    Apply lint warnings

--- feature/session/src/main/java/io/github/droidkaigi/confsched2020/session/ui/item/SessionDetailSpeakerSubtitleItem.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2020/session/ui/item/SessionDetailSpeakerSubtitleItem.kt
@@ -5,7 +5,8 @@ import com.xwray.groupie.databinding.BindableItem
 import io.github.droidkaigi.confsched2020.session.R
 import io.github.droidkaigi.confsched2020.session.databinding.ItemSessionDetailSpeakerSubtitleBinding
 
-class SessionDetailSpeakerSubtitleItem @AssistedInject constructor() : BindableItem<ItemSessionDetailSpeakerSubtitleBinding>() {
+class SessionDetailSpeakerSubtitleItem @AssistedInject constructor() :
+    BindableItem<ItemSessionDetailSpeakerSubtitleBinding>() {
     override fun getLayout() = R.layout.item_session_detail_speaker_subtitle
 
     override fun bind(binding: ItemSessionDetailSpeakerSubtitleBinding, position: Int) {
