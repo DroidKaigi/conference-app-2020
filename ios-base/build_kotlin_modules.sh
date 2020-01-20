251f9da59d936c57a3ad464b6b8490e68b586579
ryoya ito <horusu.dqx@gmail.com>
2020-01-21 00:34:47 +0900

    use sessings.gradle instead of echo

--- ios-base/build_kotlin_modules.sh
+++ ios-base/build_kotlin_modules.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-echo "include ':ios-combined'" > $SRCROOT/../ios-settings.gradle
 cd $SRCROOT/../
-./gradlew :ios-combined:linkIosX64 -PXCODE_CONFIGURATION=${CONFIGURATION}
+BUILD_IOS=true ./gradlew :ios-combined:linkIosX64 -PXCODE_CONFIGURATION=${CONFIGURATION}
