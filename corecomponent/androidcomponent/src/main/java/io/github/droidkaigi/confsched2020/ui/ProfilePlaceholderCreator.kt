1c3ea42edbde385cc56ceccc6f7e9f97a24e59b1
Miguel Kano <miguel.g.kano@gmail.com>
2020-01-20 20:07:27 -0800

    Fix lint warning

--- corecomponent/androidcomponent/src/main/java/io/github/droidkaigi/confsched2020/ui/ProfilePlaceholderCreator.kt
+++ corecomponent/androidcomponent/src/main/java/io/github/droidkaigi/confsched2020/ui/ProfilePlaceholderCreator.kt
@@ -2,7 +2,6 @@ package io.github.droidkaigi.confsched2020.ui
 
 import android.content.Context
 import androidx.appcompat.content.res.AppCompatResources
-import androidx.core.content.ContextCompat
 import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
 import io.github.droidkaigi.confsched2020.widget.component.R
 
@@ -14,6 +13,8 @@ object ProfilePlaceholderCreator {
             R.drawable.ic_person_outline_black_32dp,
             null
         )?.apply {
-            setTint(AppCompatResources.getColorStateList(context, R.color.profile_icon).defaultColor)
+            setTint(
+                AppCompatResources.getColorStateList(context, R.color.profile_icon).defaultColor
+            )
         }
 }
