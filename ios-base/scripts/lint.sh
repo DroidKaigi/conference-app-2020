57e3a8f640ec9e5efad066d5b99815d8313cf905
ryoya ito <horusu.dqx@gmail.com>
2020-01-22 03:32:21 +0900

    fix lint.sh to run swiftlint

--- ios-base/scripts/lint.sh
+++ ios-base/scripts/lint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 git diff --name-only | grep -e '\(.*\).swift$' | while read filename; do
-  mint run swiftlint --path "$filename"
+  mint run realm/swiftlint swiftlint --path "$SRCROOT/../$filename"
 done
