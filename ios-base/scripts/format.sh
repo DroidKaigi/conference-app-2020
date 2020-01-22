7e49d050c0bfa5e6d184949581de77a1b9800220
woxtu <woxtup@gmail.com>
2020-01-23 00:12:59 +0900

    Use $SRCROOT to get a file path

--- ios-base/scripts/format.sh
+++ ios-base/scripts/format.sh
@@ -2,5 +2,5 @@
 
 git diff --name-only | grep -e '\(.*\).swift$' | while read filename; do
   # ref: https://github.com/yonaskolb/Mint/issues/112
-  mint run swiftformat swiftformat "$(git rev-parse --show-toplevel)/$filename"
+  mint run swiftformat swiftformat "$SRCROOT/../$filename"
 done
