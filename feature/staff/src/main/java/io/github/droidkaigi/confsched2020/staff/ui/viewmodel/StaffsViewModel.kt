0bc6660bd4a8162dae007a19abb9b0d146469f34
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-01-22 23:48:53 +0900

    create store object

--- feature/staff/src/main/java/io/github/droidkaigi/confsched2020/staff/ui/viewmodel/StaffsViewModel.kt
+++ feature/staff/src/main/java/io/github/droidkaigi/confsched2020/staff/ui/viewmodel/StaffsViewModel.kt
@@ -4,17 +4,25 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.liveData
+import com.dropbox.android.external.store4.MemoryPolicy
+import com.dropbox.android.external.store4.StoreBuilder
 import io.github.droidkaigi.confsched2020.data.api.DroidKaigiApi
+import io.github.droidkaigi.confsched2020.data.api.response.StaffResponse
 import io.github.droidkaigi.confsched2020.data.db.StaffDatabase
+import io.github.droidkaigi.confsched2020.data.db.entity.StaffEntity
 import io.github.droidkaigi.confsched2020.ext.combine
 import io.github.droidkaigi.confsched2020.ext.toAppError
 import io.github.droidkaigi.confsched2020.ext.toLoadingState
 import io.github.droidkaigi.confsched2020.model.AppError
 import io.github.droidkaigi.confsched2020.model.LoadState
+import io.github.droidkaigi.confsched2020.model.Staff
 import io.github.droidkaigi.confsched2020.model.StaffContents
 import io.github.droidkaigi.confsched2020.model.repository.StaffRepository
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
+@FlowPreview
 class StaffsViewModel @Inject constructor(
     private val api: DroidKaigiApi,
     private val staffDatabase: StaffDatabase,
@@ -30,6 +38,14 @@ class StaffsViewModel @Inject constructor(
             val EMPTY = UiModel(false, null, StaffContents.EMPTY)
         }
     }
+
+    private val store = StoreBuilder.fromNonFlow<Unit, StaffResponse> { api.getStaffs() }
+        .persister(
+            reader = { readFromLocal() },
+            writer = { _: Unit, output: StaffResponse -> staffDatabase.save(output) }
+        )
+        .cachePolicy(MemoryPolicy.builder().build())
+        .build()
 
     private val staffContentsLoadState: LiveData<LoadState<StaffContents>> = liveData {
         emitSource(
@@ -58,5 +74,11 @@ class StaffsViewModel @Inject constructor(
             staffContents = staffContents
         )
     }
+
+    private fun readFromLocal() = staffDatabase
+        .staffs()
+        .map { StaffContents(it.map { staffEntity -> staffEntity.toStaff() }) }
+
+    private fun StaffEntity.toStaff(): Staff = Staff(id, name, iconUrl, profileUrl)
 
 }
