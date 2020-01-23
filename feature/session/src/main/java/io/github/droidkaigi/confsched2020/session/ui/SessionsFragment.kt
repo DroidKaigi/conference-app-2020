8f9df1427f32a4bf2ddb5dd585c5246c2f6a9ac8
Akita <hirotaka.akita@fuller.co.jp>
2020-01-23 10:00:49 +0900

    add comment

--- feature/session/src/main/java/io/github/droidkaigi/confsched2020/session/ui/SessionsFragment.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2020/session/ui/SessionsFragment.kt
@@ -113,6 +113,8 @@ class SessionsFragment : DaggerFragment() {
         binding.sessionsSheet.doOnApplyWindowInsets { _, insets, _ ->
             sessionSheetBehavior.peekHeight =
                 insets.systemWindowInsetBottom + initialPeekHeight + gestureNavigationBottomSpace
+            // This block is the workaround to bottomSheetBehavior bug fix.
+            // https://stackoverflow.com/questions/35685681/dynamically-change-height-of-bottomsheetbehavior
             if (sessionSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED)
                 sessionSheetBehavior.onLayoutChild(
                     binding.fragmentSessionsCoordinator,
