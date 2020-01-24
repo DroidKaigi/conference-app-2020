c7b71467079040b6a4733a73f7d3d938f5685dfd
Masatoshi Kubode <masatoshi@wantedly.com>
2020-01-24 20:29:35 +0900

    Remove autoCleared from SessionsFragment

--- feature/session/src/main/java/io/github/droidkaigi/confsched2020/session/ui/SessionsFragment.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2020/session/ui/SessionsFragment.kt
@@ -8,11 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.addCallback
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.children
 import androidx.core.view.updatePadding
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentContainerView
+import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.observe
@@ -39,21 +38,12 @@ import io.github.droidkaigi.confsched2020.session.ui.viewmodel.SessionTabViewMod
 import io.github.droidkaigi.confsched2020.session.ui.viewmodel.SessionsViewModel
 import io.github.droidkaigi.confsched2020.ui.widget.FilterChip
 import io.github.droidkaigi.confsched2020.ui.widget.onCheckedChanged
-import io.github.droidkaigi.confsched2020.util.autoCleared
 import javax.inject.Inject
 import javax.inject.Provider
 
 class SessionsFragment : DaggerFragment() {
 
-    private var binding: FragmentSessionsBinding by autoCleared()
     private lateinit var overrideBackPressedCallback: OnBackPressedCallback
-
-    private val sessionSheetBehavior: BottomSheetBehavior<View>
-        get() {
-            val layoutParams = binding.sessionsSheet.layoutParams as CoordinatorLayout.LayoutParams
-            val behavior = layoutParams.behavior
-            return (behavior as BottomSheetBehavior)
-        }
 
     @Inject
     lateinit var sessionsViewModelProvider: Provider<SessionsViewModel>
@@ -91,19 +81,20 @@ class SessionsFragment : DaggerFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(
-            inflater,
+        setHasOptionsMenu(true)
+        return inflater.inflate(
             R.layout.fragment_sessions,
             container,
             false
         )
-        setHasOptionsMenu(true)
-        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initBottomSheetShapeAppearance()
+        val binding = FragmentSessionsBinding.bind(view)
+        val sessionSheetBehavior = BottomSheetBehavior.from(binding.sessionsSheet)
+
+        initBottomSheetShapeAppearance(binding)
         val initialPeekHeight = sessionSheetBehavior.peekHeight
         val gestureNavigationBottomSpace =
             if (isEdgeToEdgeEnabled())
@@ -223,13 +214,14 @@ class SessionsFragment : DaggerFragment() {
                 fragmentContainerView(binding.root)?.requestLayout()
             }
         }
-    }
 
-    override fun onResume() {
-        super.onResume()
-        // override back button iff front layer collapsed (filter is shown)
-        overrideBackPressedCallback.isEnabled =
-            sessionSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED
+        viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
+            override fun onResume(owner: LifecycleOwner) {
+                // override back button iff front layer collapsed (filter is shown)
+                overrideBackPressedCallback.isEnabled =
+                    sessionSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED
+            }
+        })
     }
 
     override fun onPause() {
@@ -315,7 +307,7 @@ class SessionsFragment : DaggerFragment() {
      * Override Widget.MaterialComponents.BottomSheet shapeAppearance
      * see: https://github.com/DroidKaigi/conference-app-2020/issues/104
      */
-    private fun initBottomSheetShapeAppearance() {
+    private fun initBottomSheetShapeAppearance(binding: FragmentSessionsBinding) {
         val shapeAppearanceModel =
             ShapeAppearanceModel.Builder()
                 .setTopLeftCorner(
