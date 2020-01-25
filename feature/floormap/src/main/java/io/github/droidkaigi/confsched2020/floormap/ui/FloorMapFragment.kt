e6b19d66190cd26436174fe0d3fca4faed6d50fc
Veget-ables <makuhari109@gmail.com>
2020-01-25 22:48:48 +0900

    Set an ic_image into PhotoView according to the floorMap

--- feature/floormap/src/main/java/io/github/droidkaigi/confsched2020/floormap/ui/FloorMapFragment.kt
+++ feature/floormap/src/main/java/io/github/droidkaigi/confsched2020/floormap/ui/FloorMapFragment.kt
@@ -6,14 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import androidx.navigation.fragment.navArgs
 import dagger.Module
 import dagger.Provides
 import dagger.android.support.DaggerFragment
 import io.github.droidkaigi.confsched2020.di.PageScope
 import io.github.droidkaigi.confsched2020.floormap.R
+import io.github.droidkaigi.confsched2020.floormap.databinding.FragmentFloormapBinding
+import io.github.droidkaigi.confsched2020.model.FloorMap
 
 // TODO: Apply the floor map UI
 class FloorMapFragment : DaggerFragment() {
+
+    private val navArgs: FloorMapFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +30,26 @@ class FloorMapFragment : DaggerFragment() {
             container,
             false
         )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentFloormapBinding.bind(view)
+
+        binding.floorMapImage.setImageResource(getFloorMapResource())
+    }
+
+    private fun getFloorMapResource(): Int {
+        val floor = FloorMap.fromId(navArgs.roomId) ?: return R.drawable.ic_floor_map
+        return when (floor) {
+            FloorMap.APP_BAR -> R.drawable.ic_floor_map_app_bars
+            FloorMap.BACKDROP -> R.drawable.ic_floor_map_backdrop
+            FloorMap.CARDS -> R.drawable.ic_floor_map_cards
+            FloorMap.DIALOGS -> R.drawable.ic_floor_map_dialogs
+            FloorMap.PICKERS -> R.drawable.ic_floor_map_pickers
+            FloorMap.SLIDERS -> R.drawable.ic_floor_map_sliders
+            FloorMap.TABS -> R.drawable.ic_floor_map_tabs
+        }
     }
 
     @Module
--- model/src/commonMain/kotlin/io/github/droidkaigi/confsched2020/model/FloorMap.kt
+++ model/src/commonMain/kotlin/io/github/droidkaigi/confsched2020/model/FloorMap.kt
@@ -1,6 +1,17 @@
 package io.github.droidkaigi.confsched2020.model
 
-enum class FloorMap(val title: String) {
-    FLOOR_1("1F"),
-    FLOOR_5("5F")
+enum class FloorMap(val id: Int) {
+    APP_BAR(11511),
+    BACKDROP(11512),
+    CARDS(11513),
+    DIALOGS(11514),
+    PICKERS(11515),
+    SLIDERS(11516),
+    TABS(11517);
+
+    companion object {
+        fun fromId(id: Int): FloorMap? {
+            return values().firstOrNull { it.id == id }
+        }
+    }
 }
