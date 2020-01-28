0e85f8025eef89d9c1462d1786ed83a56877dcff
417-72KI <417.72ki@gmail.com>
2020-01-28 10:51:28 +0900

    Show title

--- ios-base/DroidKaigi 2020/Session/Detail/SessionDetailViewController.swift
+++ ios-base/DroidKaigi 2020/Session/Detail/SessionDetailViewController.swift
@@ -4,8 +4,18 @@ import UIKit
 final class SessionDetailViewController: UIViewController {
     var session: Session!
 
+    @IBOutlet private weak var titleLabel: UILabel!
+
+    // MARK: Life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(session)
+
+        loadSession(session)
+    }
+}
+
+private extension SessionDetailViewController {
+    func loadSession(_ session: Session) {
+        titleLabel.text = session.title.ja
     }
 }
