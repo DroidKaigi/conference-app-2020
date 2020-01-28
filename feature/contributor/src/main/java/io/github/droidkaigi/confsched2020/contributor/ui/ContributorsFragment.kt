c1d5dc7b1b0fb11b16b2c2a3d389882e0adad51d
saccho <saccho956@yahoo.co.jp>
2020-01-28 19:21:26 +0900

    Update bottom padding for contributor

--- feature/contributor/src/main/java/io/github/droidkaigi/confsched2020/contributor/ui/ContributorsFragment.kt
+++ feature/contributor/src/main/java/io/github/droidkaigi/confsched2020/contributor/ui/ContributorsFragment.kt
@@ -3,6 +3,7 @@ package io.github.droidkaigi.confsched2020.contributor.ui
 import android.os.Bundle
 import android.view.View
 import androidx.core.view.isVisible
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
@@ -16,6 +17,7 @@ import com.xwray.groupie.databinding.ViewHolder
 import dagger.Component
 import dagger.Module
 import dagger.Provides
+import dev.chrisbanes.insetter.doOnApplyWindowInsets
 import io.github.droidkaigi.confsched2020.App
 import io.github.droidkaigi.confsched2020.contributor.R
 import io.github.droidkaigi.confsched2020.contributor.databinding.FragmentContributorsBinding
@@ -88,6 +90,17 @@ class ContributorsFragment : Fragment(R.layout.fragment_contributors) {
                     binding.contributor,
                     it,
                     binding.retryButton.visibility != View.VISIBLE
+                )
+            }
+        }
+
+        // Set a bottom padding due to the system UI is enabled.
+        binding.contributorRecycler.run {
+            adapter = groupAdapter
+            doOnApplyWindowInsets { recyclerView, insets, initialState ->
+                // Set a bottom padding due to the system UI is enabled.
+                recyclerView.updatePadding(
+                    bottom = insets.systemWindowInsetBottom + initialState.paddings.bottom
                 )
             }
         }
