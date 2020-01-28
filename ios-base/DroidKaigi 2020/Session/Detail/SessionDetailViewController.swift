2c9fabf078f52350ea894f4ac397514056423fcb
417-72KI <417.72ki@gmail.com>
2020-01-28 10:51:36 +0900

    Description

--- ios-base/DroidKaigi 2020/Session/Detail/SessionDetailViewController.swift
+++ ios-base/DroidKaigi 2020/Session/Detail/SessionDetailViewController.swift
@@ -1,18 +1,29 @@
 import ios_combined
+import RxCocoa
+import RxSwift
 import UIKit
 
 final class SessionDetailViewController: UIViewController {
     var session: Session!
 
+    // MARK: Private
+
+    private let disposeBag = DisposeBag()
+
+    // MARK: Outlets
+
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var dateLabel: UILabel!
     @IBOutlet private weak var timeRoomLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    @IBOutlet private weak var readMoreButton: UIButton!
 
     // MARK: Life cycles
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        bind()
         loadSession(session)
     }
 }
@@ -22,5 +33,17 @@ private extension SessionDetailViewController {
         titleLabel.text = session.title.ja
         dateLabel.text = session.startDayText
         timeRoomLabel.text = session.timeRoomText
+        descriptionLabel.text = session.desc
+        print(session)
+    }
+
+    func bind() {
+        disposeBag.insert(
+            readMoreButton.rx.tap
+                .bind(onNext: { [readMoreButton, descriptionLabel] in
+                    descriptionLabel?.numberOfLines = 0
+                    readMoreButton?.isHidden = true
+                })
+        )
     }
 }
