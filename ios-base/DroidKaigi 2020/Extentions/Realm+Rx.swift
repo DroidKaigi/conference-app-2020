8b3e7c0ed449b82d455eb6da6e0f59903ed2aede
fummicc1 <fumiya989@gmail.com>
2020-01-31 02:05:42 +0900

    Add Fetch method for realm

--- ios-base/DroidKaigi 2020/Extentions/Realm+Rx.swift
+++ ios-base/DroidKaigi 2020/Extentions/Realm+Rx.swift
@@ -4,25 +4,41 @@ import RxSwift
 extension Realm: ReactiveCompatible {}
 
 extension Reactive where Base == Realm {
-	
-	func write(object: Object) -> Completable {
-		let config = base.configuration
-		return Completable.create { observer -> Disposable in
-			autoreleasepool {
-				DispatchQueue.global().async {
-					do {
-						let realm: Realm = try Realm(configuration: config)
-						try realm.write {
-							realm.add(object)
-						}
-						observer(.completed)
-					} catch {
-						observer(.error(error))
-					}
-				}
-			}
-			return Disposables.create()
-		}
-	}
-	
+    func write(object: Object) -> Completable {
+        let config = base.configuration
+        return Completable.create { observer -> Disposable in
+            autoreleasepool {
+                DispatchQueue.global().async {
+                    do {
+                        let realm: Realm = try Realm(configuration: config)
+                        try realm.write {
+                            realm.add(object)
+                        }
+                        observer(.completed)
+                    } catch {
+                        observer(.error(error))
+                    }
+                }
+            }
+            return Disposables.create()
+        }
+    }
+
+    func fetch<T: Object>() -> Single<[T]> {
+        let config = base.configuration
+        return Single.create { observer -> Disposable in
+            autoreleasepool {
+                DispatchQueue.global().async {
+                    do {
+                        let realm: Realm = try Realm(configuration: config)
+                        let objects = Array(realm.objects(T.self))
+                        observer(.success(objects))
+                    } catch {
+                        observer(.error(error))
+                    }
+                }
+            }
+            return Disposables.create()
+        }
+    }
 }
