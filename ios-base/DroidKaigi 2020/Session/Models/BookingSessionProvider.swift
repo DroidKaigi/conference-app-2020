0f35ea4927110fc71b4a42fbeb2f5903a0521f02
fummicc1 <fumiya989@gmail.com>
2020-01-31 01:24:48 +0900

    implement bookSession function

--- ios-base/DroidKaigi 2020/Session/Models/BookingSessionProvider.swift
+++ ios-base/DroidKaigi 2020/Session/Models/BookingSessionProvider.swift
@@ -3,6 +3,13 @@ import RealmSwift
 import RxSwift
 
 final class BookingSessionProvider {
-	func bookSession(_ sessionID: Int) {
-	}
+    func bookSession(session: Session) -> Completable {
+        do {
+            let realm = try Realm()
+            let localSession = LocalSession(session: session)
+            return realm.rx.write(object: localSession)
+        } catch {
+            return .error(error)
+        }
+    }
 }
