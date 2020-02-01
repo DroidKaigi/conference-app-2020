bb98a774024a97eb62129c6843895bfe014b8ce0
clockvoid <hidenari.t1997nov@gmail.com>
2020-02-02 06:57:51 +0900

    add: eventviewcontroller

---
+++ ios-base/DroidKaigi 2020/Session/Views/EventViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EventViewController.swift
+//  DroidKaigi 2020
+//
+//  Created by Hidenari Tajima on 2020/02/02.
+//
+
+import UIKit
+
+class EventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
