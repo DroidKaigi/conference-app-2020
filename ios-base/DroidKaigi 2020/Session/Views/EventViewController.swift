ef413fb420dffe1c3e7b605d09f5251a7a0046ab
clockvoid <hidenari.t1997nov@gmail.com>
2020-02-03 13:07:28 +0900

    remove: sign on EventViewController.swift

--- ios-base/DroidKaigi 2020/Session/Views/EventViewController.swift
+++ ios-base/DroidKaigi 2020/Session/Views/EventViewController.swift
@@ -1,16 +1,7 @@
-//
-//  EventViewController.swift
-//  DroidKaigi 2020
-//
-//  Created by Hidenari Tajima on 2020/02/02.
-//
-
 import UIKit
 
 class EventViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
 }
