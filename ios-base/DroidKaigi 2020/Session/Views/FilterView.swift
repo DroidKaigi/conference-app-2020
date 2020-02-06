55c50ba6d97d9b20d3a66d1a165456839a453e88
ryoya ito <horusu.dqx@gmail.com>
2020-02-07 05:07:40 +0900

    align cells to left

--- ios-base/DroidKaigi 2020/Session/Views/FilterView.swift
+++ ios-base/DroidKaigi 2020/Session/Views/FilterView.swift
@@ -3,13 +3,14 @@ import UIKit
 
 final class FilterView: CollectionView {
     override init() {
-        let layout = UICollectionViewFlowLayout()
+        let layout = CollectionViewFlowLayoutLeftAlign()
         layout.estimatedItemSize = .init(width: ChipCell.estimatedCellWidth, height: ChipCell.cellHeight)
         layout.headerReferenceSize = .init(width: UIScreen.main.bounds.width, height: 60)
         super.init(frame: .zero, collectionViewLayout: layout)
         register(UINib(nibName: "FilterSectionHeaderView", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "SectionHeader")
         register(UINib(nibName: "ChipCell", bundle: nil), forCellWithReuseIdentifier: "ChipCell")
 
+        contentInset = .init(top: 0, left: 24, bottom: 0, right: 24)
         backgroundColor = Asset.primary.color
     }
 
