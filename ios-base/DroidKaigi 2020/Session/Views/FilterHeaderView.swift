e35a1fe24aa18e00740e65b3f47b0058c802dcb0
ryoya ito <horusu.dqx@gmail.com>
2020-02-07 18:23:12 +0900

    localize header labels

--- ios-base/DroidKaigi 2020/Session/Views/FilterHeaderView.swift
+++ ios-base/DroidKaigi 2020/Session/Views/FilterHeaderView.swift
@@ -2,11 +2,17 @@ import Material
 import UIKit
 
 final class FilterHeaderView: UIView {
-    @IBOutlet weak var filterLabel: UILabel!
+    @IBOutlet weak var filterLabel: UILabel! {
+        didSet {
+            filterLabel.text = L10n.filter
+        }
+    }
+
     @IBOutlet weak var resetButton: Button! {
         didSet {
             resetButton.layer.cornerRadius = 4
             resetButton.clipsToBounds = true
+            resetButton.setTitle(L10n.reset, for: .normal)
         }
     }
 
@@ -22,8 +28,8 @@ final class FilterHeaderView: UIView {
 
     func loadNib() {
         if let view = Bundle.main.loadNibNamed("FilterHeaderView", owner: self, options: nil)?.first as? UIView {
-            view.frame = self.bounds
-            self.addSubview(view)
+            view.frame = bounds
+            addSubview(view)
         }
     }
 }
