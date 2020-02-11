ba6c8d3ae1020a1e5912c296ad49f7803b254b24
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2020-02-11 19:56:04 +0900

    Add: fetch map image from server

--- ios-base/DroidKaigi 2020/FloorMap/FloorMapViewController.swift
+++ ios-base/DroidKaigi 2020/FloorMap/FloorMapViewController.swift
@@ -1,4 +1,5 @@
 import ios_combined
+import Nuke
 import UIKit
 
 final class FloorMapViewController: ContentViewController {
@@ -13,5 +14,24 @@ final class FloorMapViewController: ContentViewController {
         super.viewDidLoad()
 
         title = L10n.floorMaps
+        loadMap()
+    }
+}
+
+// MARK: - Private functions
+
+private extension FloorMapViewController {
+    func loadMap() {
+        let width = imageView.image?.size.width
+        let height = imageView.image?.size.height
+
+        let urlString: String = "https://api.droidkaigi.jp/images/2020/map.png"
+        if let url = URL(string: urlString) {
+            let request = ImageRequest(
+                url: url,
+                processors: [ImageProcessor.Resize(size: CGSize(width: width!, height: height!))] // Set target size in pixels
+            )
+            Nuke.loadImage(with: request, into: imageView)
+        }
     }
 }
