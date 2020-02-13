466e4a55b5caca1d6a72d2a65a601a3587f61d5b
takahirom <takam.dev@gmail.com>
2020-02-13 22:02:19 +0900

    Fix bugs

--- feature/session/src/main/java/io/github/droidkaigi/confsched2020/session/ui/widget/BottomSheetMotionLayout.kt
+++ feature/session/src/main/java/io/github/droidkaigi/confsched2020/session/ui/widget/BottomSheetMotionLayout.kt
@@ -26,4 +26,40 @@ class BottomSheetMotionLayout @JvmOverloads constructor(
             }
         })
     }
+
+
+    private var undergoingMotion: Boolean = false
+
+    override fun onNestedScroll(
+        target: View,
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        type: Int,
+        consumed: IntArray
+    ) {
+        // super.onNestedScroll(...) {
+        //     consumed[0] += dxUnconsumed;
+        //     consumed[1] += dyUnconsumed;
+        // }
+
+        // Only add to consumed if we are still undergoing motion, or a child is consuming the scroll
+        if (undergoingMotion || dxConsumed != 0 || dyConsumed != 0) {
+            consumed[0] += dxUnconsumed
+            consumed[1] += dyUnconsumed
+        }
+        // Reset undergoingMotion, as onNestedPreScroll will always set this back to true if we are still undergoing
+        // motion
+        undergoingMotion = false
+
+    }
+
+    override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
+        super.onNestedPreScroll(target, dx, dy, consumed, type)
+        // If consumed is non-zero, then this MotionLayout is changing its progress
+        if (consumed[0] != 0 || consumed[1] != 0) {
+            undergoingMotion = true
+        }
+    }
 }
