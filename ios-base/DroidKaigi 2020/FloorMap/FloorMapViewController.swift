cad1f1e4bcf609c4b02c45bfb1934bfbc62c26c4
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2020-02-13 20:27:36 +0900

    Improve: floor map caching

--- ios-base/DroidKaigi 2020/FloorMap/FloorMapViewController.swift
+++ ios-base/DroidKaigi 2020/FloorMap/FloorMapViewController.swift
@@ -29,19 +29,39 @@ final class FloorMapViewController: ContentViewController {
 // MARK: - Private functions
 
 private extension FloorMapViewController {
+    
+    /// This method caches the floor map image in LRU memory when loading from server.
     func loadMap() {
-        let width = imageView.image?.size.width
-        let height = imageView.image?.size.height
-
+        // Configure cache
+        ImageCache.shared.costLimit = 1024 * 1024 * 100 // 100 MB
+        ImageCache.shared.countLimit = 100
+        ImageCache.shared.ttl = 120 // Invalidate image after 120 sec
+        
+        ImageLoadingOptions.shared.failureImage = UIImage(named: "map") // The set image is applied when reading from the server fails
+        
         let urlString: String = "https://api.droidkaigi.jp/images/2020/map.png"
         if let url = URL(string: urlString) {
+            let width = imageView.image?.size.width
+            let height = imageView.image?.size.height
+            
             let request = ImageRequest(
                 url: url,
                 processors: [ImageProcessor.Resize(size: CGSize(width: width!, height: height!))] // Set target size in pixels
             )
-            let options = ImageLoadingOptions(
-                failureImage: UIImage(named: "map")) // The set image is applied when reading from the server fails
-            Nuke.loadImage(with: request, options: options, into: imageView)
+            
+            if ImageCache.shared[request] !=  nil {
+                self.imageView.image = ImageCache.shared[request]
+            } else {
+                ImagePipeline.shared.loadImage(with: url) { result in
+                    switch result {
+                        case let .success(response):
+                            ImageCache.shared[request] = response.image // Set cache
+                            self.imageView.image = ImageCache.shared[request]
+                        case .failure:
+                            self.imageView.image = ImageLoadingOptions.shared.failureImage
+                    }
+                }
+            }
         }
     }
 }
