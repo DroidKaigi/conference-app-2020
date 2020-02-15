e556cacb388fb68a68d3d24b9807901b5ca413b6
takahirom <takam.dev@gmail.com>
2020-02-15 18:13:15 +0900

    Use UiModel.error

--- model/src/commonMain/kotlin/io/github/droidkaigi/confsched2020/model/LoadState.kt
+++ model/src/commonMain/kotlin/io/github/droidkaigi/confsched2020/model/LoadState.kt
@@ -1,10 +1,10 @@
 package io.github.droidkaigi.confsched2020.model
 
-interface errorGettable {
+interface ErrorGettable {
     fun getErrorIfExists(): Throwable?
 }
 
-sealed class LoadState<out T> : errorGettable {
+sealed class LoadState<out T> : ErrorGettable {
     object Loading : LoadState<Nothing>()
     class Loaded<T>(val value: T) : LoadState<T>()
     class Error<T>(val e: Throwable) : LoadState<T>()
@@ -14,11 +14,26 @@ sealed class LoadState<out T> : errorGettable {
     fun getValueOrNull(): T? = if (this is Loaded<T>) value else null
 }
 
-sealed class LoadingState : errorGettable {
+sealed class LoadingState : ErrorGettable {
     object Loading : LoadingState()
     object Loaded : LoadingState()
     class Error(val e: Throwable) : LoadingState()
 
     val isLoading get() = this is Loading
     override fun getErrorIfExists() = if (this is Error) e else null
+}
+
+sealed class ResultState<T> : ErrorGettable {
+    class Success<T>(val value: T) : ResultState<T>()
+    class Error<T>(val e: Throwable) : ResultState<T>()
+
+    override fun getErrorIfExists() = if (this is Error) e else null
+    fun getOrDefault(default: T): T {
+        if (this is Success) return value
+        return default
+    }
+}
+
+fun List<ErrorGettable>.firstErrorOrNull(): Throwable? {
+    return mapNotNull { it.getErrorIfExists() }.firstOrNull()
 }
