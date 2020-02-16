7836aa54549a28f43e6fe2a1c152f0e94bf9feb0
Takahiro Menju <takam.dev@gmail.com>
2020-02-16 09:10:19 +0900

    Update version

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -7,7 +7,7 @@ object Versions {
 
     private val versionMajor = 1
     private val versionMinor = 0
-    private val versionPatch = 0
+    private val versionPatch = 1
     private val versionOffset = 1
     val androidVersionCode =
         ((1 + versionMajor) * 10000 + versionMinor * 100 + versionPatch) * 100 + versionOffset
