5e7ab9dc76359ef606600a9e785202e8b7da04da
IppeiMukaida <ippei-mukaida@cybozu.co.jp>
2020-02-16 16:06:47 +0900

    Fix error if build directory is already exists

--- ios-base/scripts/build_kotlin_modules.sh
+++ ios-base/scripts/build_kotlin_modules.sh
@@ -2,5 +2,5 @@
 
  cd $SRCROOT/../
  BUILD_IOS=true ./gradlew :ioscombined:$KN_LIBRARY_BUILD_TASK -PXCODE_CONFIGURATION=${CONFIGURATION}
- mkdir $SRCROOT/build
+ mkdir -p $SRCROOT/build
  cp -r $KN_LIBRARY_BUILD_PATH $SRCROOT/build
