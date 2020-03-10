8b344677c5e7b0c8b547ecfd3ec0103f15713242
takahirom <takam.dev@gmail.com>
2020-03-10 04:33:52 +0000

    Upgraded androidx.core:core-ktx from 1.2.0-rc01 to 1.2.0

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -49,7 +49,7 @@ object Dep {
         val constraint = "androidx.constraintlayout:constraintlayout:2.0.0-beta4"
         val emoji = "androidx.emoji:emoji-appcompat:1.0.0"
         val design = "com.google.android.material:material:1.2.0-alpha05"
-        val coreKtx = "androidx.core:core-ktx:1.2.0-rc01"
+        val coreKtx = "androidx.core:core-ktx:1.2.0"
         val preference = "androidx.preference:preference:1.1.0"
         val browser = "androidx.browser:browser:1.2.0"
         val activityKtx = "androidx.activity:activity-ktx:1.1.0"
