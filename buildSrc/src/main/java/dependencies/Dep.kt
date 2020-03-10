24841387bb44afc81c4f6e257b6cff367a5e802f
takahirom <takam.dev@gmail.com>
2020-03-10 04:34:06 +0000

    Upgraded androidx.room:room-ktx from 2.2.3 to 2.2.4

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -62,7 +62,7 @@ object Dep {
         object Room {
             val compiler = "androidx.room:room-compiler:2.2.4"
             val runtime = "androidx.room:room-runtime:2.2.3"
-            val coroutine = "androidx.room:room-ktx:2.2.3"
+            val coroutine = "androidx.room:room-ktx:2.2.4"
         }
 
         object Navigation {
