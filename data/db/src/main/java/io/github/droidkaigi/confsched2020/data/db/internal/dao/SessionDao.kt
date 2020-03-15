5ea4077b52d7a17a412943c3d142798ec13c7bab
takahirom <takam.dev@gmail.com>
2020-03-15 18:15:45 +0900

    Fix Room library compile error

--- data/db/src/main/java/io/github/droidkaigi/confsched2020/data/db/internal/dao/SessionDao.kt
+++ data/db/src/main/java/io/github/droidkaigi/confsched2020/data/db/internal/dao/SessionDao.kt
@@ -5,14 +5,15 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import io.github.droidkaigi.confsched2020.data.db.internal.entity.SessionEntityImpl
 
 @Dao
 internal abstract class SessionDao {
-    @Query("SELECT * FROM session")
+    @Transaction @Query("SELECT * FROM session")
     abstract fun sessionsLiveData(): LiveData<List<SessionEntityImpl>>
 
-    @Query("SELECT * FROM session")
+    @Transaction @Query("SELECT * FROM session")
     abstract fun sessions(): List<SessionEntityImpl>
 
     @Query("DELETE FROM session")
